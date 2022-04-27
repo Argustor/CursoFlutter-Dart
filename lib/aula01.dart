52f19ad3598a012208ddf69e1e1dabf3d8d9992f
jgdml <jgdml2801@gmail.com>
2022-04-27 16:04:15 -0300

    update aula 1

--- lib/aula01.dart
+++ lib/aula01.dart
@@ -10,7 +10,7 @@ void show() {
   print("Seu nome é: $nome");
 
   // Variavel int
-  // operadores aritmeticos: + - * / %
+  // operadores aritmeticos: +, -, *, /, ~/, %
   int numero = 5;
   int resultado = numero + 5;
   print("Resultado: $resultado");
@@ -19,77 +19,74 @@ void show() {
   double res = numero / 2;
   print("Resultado: $res");
 
-  // variavel bool
-  var idade = 20;
-  // idade = "vinte"; erro
-  bool deMaior = true;
-  deMaior = idade > 17;
-  print(deMaior);
 
-  // if
-  if (deMaior){
-    print('É de maior');
-  }
-  else{
-    print('É de menor');
-  }
 
-  // comparação pode ser no proprio if sem variavel
-  if (idade > 17){
-    print('É de maior');
-  }
-  else{
-    print('É de menor');
-  }
-
-  // if ternario
-  idade > 17 ? print('É de maior') : print('É de menor');
+  // exemplo if
+  int idade = 14;
   
-  print(idade > 17 ? 'É de maior' : 'É de menor');
-
-
-  // atividade - maior numero
-  int n1 = 10;
-  int n2 = 8;
-
-  if (n1 > n2){
-    print('O maior é $n1');
-  }
-  else {
-    print('O maior é $n2');
+  if (idade >= 18) {
+    print("É de maior");
+  } else {
+    print("É de menor");
   }
 
-  // módulo exemplo
-  int n3 = 5;
-  if (n3 % 2 == 0){
-    print('$n3 é par');
-  }
-  else{
-    print('$n3 é ímpar');
+  // declare duas variaveis int com valores
+  // e mostre o resto da divisão delas na tela
+  int num1 = 79;
+  int num2 = 23;
+
+  print("O resto foi: ${num1 % num2}");
+
+  int a = 10;
+  int b = 10;
+
+  if (a > b) {
+    print(a);
+  } else {
+    if (b > a) {
+      print(b);
+    } else {
+      print("São iguais");
+    }
   }
 
-  // else if
-  if (idade < 14){
+
+  // com a variavel idade acima, mostre na tela se
+  // a pessoa é criança, adolescente, adulto ou idoso
+  if (idade < 14) {
     print("Criança");
-  }
-  else if (idade < 20){
+  } else if (idade < 20) {
     print("Adolescente");
-  }
-  else {
+  } else if (idade < 60) {
     print("Adulto");
+  } else {
+    print("Idoso");
+  } 
+
+  // faça um algoritmo que tenha 3 variaveis int com notas
+  // faça a média desses números e mostre o equivalente da média em conceito
+  // sendo que: 
+  // se a media for menor que 60: D
+  // se a media for menor que 75: C
+  // se a media for menor que 85: B
+  // se a media for maior que 85: A
+
+  int n1 = 70;
+  int n2 = 80;
+  int n3 = 80;
+
+  double media = (n1 + n2 + n3) / 3;
+  print(media);
+
+  if (media < 60) {
+    print("D");
+  } else if (media < 75) {
+    print("C");
+  } else if (media < 85) {
+    print("B");
+  } else {
+    print("A");
   }
 
 
-  // atividade - maior, menor ou igual a zero
-  int n = 2;
-  if (n > 0){
-    print("$n é maior que 0");
-  }
-  else if (n < 0){
-    print("$n é menor que 0");
-  }
-  else{
-    print("O número é 0");
-  }
-  
 }
