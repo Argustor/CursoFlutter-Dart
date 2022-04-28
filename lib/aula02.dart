09bec9f6d6fb810e900227f56b55d333d4d53d57
jgdml <jgdml2801@gmail.com>
2022-04-28 18:00:42 -0300

    aula 2 update

--- lib/aula02.dart
+++ lib/aula02.dart
@@ -11,39 +11,40 @@ void show(){
   contador = 1;
   while(contador <= 10){
     print('5 * $contador = ${contador*5}');
-    contador++;
+    contador+=1;
   }
 
 
   // for exemplo
-  for (int contador = 0; contador <= 10; contador++){
+  for (int contador = 0; contador <= 10; contador+=1){
     print(contador);
   }
 
   // for progressão
   int n = 30;
   int resultado = 0;
-  for (int contador = 0; contador <= resultado; contador++){
+  for (int contador = 0; contador <= n; contador+=1){
     resultado+=contador;
   }
   print(resultado);
 
-  // lista
-  List<String> ex = <String>[];
   
-  // var
-  var itens = <String>[];
-  itens = ['a', 'b', 'c'];
-  itens.add('d');
+  // lista / var
+  var lista = [13, 59, 23, 13];
+  lista.add(4);
+  print(lista[3]);
 
   // printar itens da lista com for
-  for (int i = 0; i < itens.length; i++){
-    print(itens[i]);
+  for (int i = 0; i < lista.length; i+=1){
+    print(lista[i]);
   }
 
   // printar itens da lista com for in
-  for (var item in itens){
-    print(item);
+  for (var numero in lista) {
+    if (59 == numero) {
+      print("está na lista");
+      break;
+    }
   }
 
 
@@ -57,7 +58,7 @@ void show(){
   
   print(somarParamNome(num1: 1, num2: 1));
 
-  mostrarLista(lista: itens);
+  mostrarLista(lista: lista);
 
 
 
@@ -75,12 +76,14 @@ void show(){
     print(input);
   }
   else{
-    print("Somente números permitidos");
+    print("Este campo só permite números");
   }
   
 
   // atividades
   mostrarPares();
+  mostrarPares2();
+  encontrarNumero(lista: lista, numero: 4);
 }
 
 // funçao exemplo
@@ -121,6 +124,7 @@ void mostrarLista({required List lista}){
   }
 }
 
+// validar que o valor contem somente numeros
 bool validarNumero(String input){
   try{
     int.parse(input);
@@ -136,10 +140,16 @@ bool validarNumero(String input){
 // atividade - Faça uma função que mostre na tela todos os 
 // numeros pares de 1 a 100. Utilize for.
 void mostrarPares(){
-  for (var i = 1; i <= 100; i++){
+  for (var i = 1; i <= 100; i+=1){
     if (i % 2 == 0){
       print(i);
     }
+  }
+}
+
+void mostrarPares2(){
+  for (var i = 2; i <= 100; i+=2){
+    print(i);
   }
 }
 
@@ -147,5 +157,13 @@ void mostrarPares(){
 // atividade - Faça uma função que receba uma 
 // lista de números e um número int como parâmetro nomeado. 
 // Percorra a lista com for in e mostre na tela se o número está na lista.
-
+void encontrarNumero({required List<int> lista, required int numero}) {
+  for (var n in lista) {
+    if (n == numero) {
+      print("O número está na lista");
+      return;
+    }
+  }
+  print("O número não está na lista");
+}
 
