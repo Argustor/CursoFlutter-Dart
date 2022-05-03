85c91a0a49d73210f24bf9119d62bd9e8d1b2897
jgdml <jgdml2801@gmail.com>
2022-05-03 13:16:58 -0300

    Aula 03

--- bin/dart_atividades.dart
+++ bin/dart_atividades.dart
@@ -1,6 +1,9 @@
 import 'package:dart_atividades/aula01.dart' as a1;
 import 'package:dart_atividades/aula02.dart' as a2;
+import 'package:dart_atividades/aula03/aula03.dart' as a3;
+import 'package:dart_atividades/aula03/veiculo.dart';
 void main(List<String> arguments) {
-  a1.show();
-  a2.show();
+  // a1.show();
+  // a2.show();
+  a3.show();
 }
